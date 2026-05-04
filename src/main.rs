470d2933199296744e3d89e377c0f080c9544dcf
Amrit <amritgiri5813@gmail.com>
2026-05-04 11:31:47 +1000

    Download: Required packages, Add: counter function

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// TOTP simple version
+use hmac::{Hmac, Mac};
+use sha1::Sha1;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+type HmacSha1 = Hmac<Sha1>;
+
+fn get_counter(time_step: u64) -> u64 {
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+    start / time_step
+}
+
+fn main() {
+    println!("Hello, world!");
+}
